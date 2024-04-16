29744e402258219464674e51b94fdadd0ddead76
Omar Abdelrahman <omar.abdelrahman@commercetools.com>
2024-04-16 02:31:21 +0200

    CanOfBeans-uva, CF-939-Div-2

---
+++ tasks/CNenesMagicalMatrix.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+class CNenesMagicalMatrix {
+public:
+  void solveOne() {
+  }
+
+  void solve() {
+    int nt;
+    in >> nt;
+    for (int _ = 1; _ <= nt; _++) {
+      solveOne();
+    }
+  }
+
+  CNenesMagicalMatrix(std::istream& in, std::ostream& out)
+    : in(in), out(out) { }
+
+private:
+  std::istream& in;
+  std::ostream& out;
+};
