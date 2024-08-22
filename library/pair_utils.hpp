5e505e63568b305aca20f78bfb5a4b0abb77bb3c
Omar Abdelrahman <omar.sebres@gmail.com>
2024-08-22 11:11:36 +0200

    add add,prod,mult,div for pairs

--- library/pair_utils.hpp
+++ library/pair_utils.hpp
@@ -1,40 +1,38 @@
-template<typename U, typename V>
-std::istream& operator >> (std::istream& in, std::pair<U, V>& p) {
-  return in >> p.first >> p.second;
-}
+template<class U, class V>
+std::istream& operator>>(std::istream& in, std::pair<U, V>& p) { return in >> p.first >> p.second; }
+template<class U, class V>
+std::ostream& operator<<(std::ostream& out, const std::pair<U, V>& p) { return out << p.first << ' ' << p.second; }
 
-template<typename U, typename V>
-std::ostream& operator << (std::ostream& out, const std::pair<U, V>& p) {
-  return out << p.first << ' ' << p.second;
-}
+template<class U, class V, class T>
+inline std::pair<U, V> operator+(const std::pair<U, V>& v, const T x) { return {v.first + x, v.second + x}; }
+template<class U, class V, class T>
+inline std::pair<U, V> operator-(const std::pair<U, V>& v, const T x) { return {v.first - x, v.second - x}; }
+template<class U, class V, class T>
+inline std::pair<U, V> operator*(const std::pair<U, V>& v, const T x) { return {v.first * x, v.second * x}; }
+template<class U, class V, class T>
+inline std::pair<U, V> operator/(const std::pair<U, V>& v, const T x) { return {v.first / x, v.second / x}; }
 
-template<typename U, typename V>
-void print_pretty_pair(std::ostream& out, const std::pair<U, V>& p, const bool& new_line = true) {
-  out << '{' << p.first << ", " << p.second << '}';
+template<class U, class V, class T> inline void operator+=(std::pair<U, V>& v, const T x) { v = v + x; }
+template<class U, class V, class T> inline void operator-=(std::pair<U, V>& v, const T x) { v = v - x; }
+template<class U, class V, class T> inline void operator*=(std::pair<U, V>& v, const T x) { v = v * x; }
+template<class U, class V, class T> inline void operator/=(std::pair<U, V>& v, const T x) { v = v / x; }
+
+template<class U, class V, class A, class B>
+inline std::pair<U, V> operator+(const std::pair<U, V>& l, const std::pair<A, B>& r) { return {l.first + r.first, l.second + r.second}; }
+template<class U, class V, class A, class B>
+inline std::pair<U, V> operator-(const std::pair<U, V>& l, const std::pair<A, B>& r) { return {l.first - r.first, l.second - r.second}; }
+template<class U, class V, class A, class B>
+inline std::pair<U, V> operator*(const std::pair<U, V>& l, const std::pair<A, B>& r) { return {l.first * r.first, l.second * r.second}; }
+template<class U, class V, class A, class B>
+inline std::pair<U, V> operator/(const std::pair<U, V>& l, const std::pair<A, B>& r) { return {l.first / r.first, l.second / r.second}; }
+
+template<class U, class V, class A, class B> inline void operator+=(std::pair<U, V>& l, const std::pair<A, B>& r) { l = l + r; }
+template<class U, class V, class A, class B> inline void operator-=(std::pair<U, V>& l, const std::pair<A, B>& r) { l = l - r; }
+template<class U, class V, class A, class B> inline void operator*=(std::pair<U, V>& l, const std::pair<A, B>& r) { l = l * r; }
+template<class U, class V, class A, class B> inline void operator/=(std::pair<U, V>& l, const std::pair<A, B>& r) { l = l / r; }
+
+template<class U, class V>
+void print_pretty_pair(std::ostream& out, const std::pair<U, V>& p, const bool new_line = true) {
+  out << '(' << p.first << ", " << p.second << ')';
   if (new_line) out << '\n';
-}
-
-template<typename T>
-inline void operator += (std::pair<T, T>& v, const T& x) {
-  v.first += x;
-  v.second += x;
-}
-
-template<typename T>
-inline void operator -= (std::pair<T, T>& v, const T& x) {
-  v.first -= x;
-  v.second -= x;
-}
-
-template<typename T>
-inline void operator *= (std::pair<T, T>& v, const T& x) {
-  v.first *= x;
-  v.second *= x;
-}
-
-template<typename T>
-inline void operator /= (std::pair<T, T>& v, const T& x) {
-  assert(x != 0);
-  v.first /= x;
-  v.second /= x;
 }
---
+++ tasks/PolyominoComposer.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+class PolyominoComposer {
+public:
+  void solveOne() {
+  }
+
+  void solve() {
+    int nt;
+    in >> nt;
+    for (int _ = 1; _ <= nt; _++) {
+      solveOne();
+    }
+  }
+
+  PolyominoComposer(std::istream& in, std::ostream& out)
+    : in(in), out(out) { }
+
+private:
+  std::istream& in;
+  std::ostream& out;
+};
