e7959a140d19170376a477c4655e6865a152780c
Omar Abdelrahman <omar.sebres@gmail.com>
2024-08-25 18:21:50 +0200

    refactor adhoc_utils

--- library/adhoc_utils.hpp
+++ library/adhoc_utils.hpp
@@ -1,9 +1,20 @@
-using position = std::tuple<int, int>;
-using directions = std::vector<position>;
+using direction = std::pair<int, int>;
+using position = std::pair<int, int>;
+using directions = std::vector<direction>;
+
+const direction RIGHT = {0, 1};
+const direction LEFT = {0, -1};
+const direction UP = {-1, 0};
+const direction DOWN = {1, 0};
+
+const direction BOTTOM_LEFT = {1, 1};
+const direction BOTTOM_RIGHT = {1, -1};
+const direction TOP_LEFT = {-1, 1};
+const direction TOP_RIGHT = {-1, -1};
 
 // N, E, S, W
-directions _simple_adjacent = { { {-1, 0}, {0, 1}, {1, 0}, {0, -1} } };
-directions _all_adjacent = { { {-1, 0}, {0, 1}, {1, 0}, {0, -1}, {1, 1}, {1, -1}, {-1, 1}, {-1, -1} }};
+directions _simple_adjacent = {UP, RIGHT, DOWN, LEFT};
+directions _all_adjacent = {UP, RIGHT, DOWN, LEFT, BOTTOM_LEFT, BOTTOM_RIGHT, TOP_LEFT, TOP_RIGHT};
 
 inline bool inside(const int i, const int j, const int height, const int width) {
   return i >= 0 && i < height && j >= 0 && j < width;
@@ -26,8 +37,8 @@ void for_each_knight(const int x, const int y, const int height, const int width
 template<class Fun>
 void for_each_direction(const int x, const int y, const int height, const int width, const directions& dirs, Fun&& f) {
   for (const auto& dir : dirs) {
-    const int nx = x + std::get<0>(dir);
-    const int ny = y + std::get<1>(dir);
+    const int nx = x + dir.first;
+    const int ny = y + dir.second;
     if (inside(nx, ny, height, width))
       f(nx, ny);
   }
