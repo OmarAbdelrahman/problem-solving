a18cb44fb33250dde7b961c4e60d2e84ee7ce841
Omar Abdelrahman <omar.sebres@gmail.com>
2024-08-25 18:23:44 +0200

    avoid copying and modify in-place for +=, -=, *=, and /= operations

--- library/pair_utils.hpp
+++ library/pair_utils.hpp
@@ -12,10 +12,10 @@ inline std::pair<U, V> operator*(const std::pair<U, V>& v, const T x) { return {
 template<class U, class V, class T>
 inline std::pair<U, V> operator/(const std::pair<U, V>& v, const T x) { return {v.first / x, v.second / x}; }
 
-template<class U, class V, class T> inline void operator+=(std::pair<U, V>& v, const T x) { v = v + x; }
-template<class U, class V, class T> inline void operator-=(std::pair<U, V>& v, const T x) { v = v - x; }
-template<class U, class V, class T> inline void operator*=(std::pair<U, V>& v, const T x) { v = v * x; }
-template<class U, class V, class T> inline void operator/=(std::pair<U, V>& v, const T x) { v = v / x; }
+template<class U, class V, class T> inline void operator+=(std::pair<U, V>& v, const T x) { v.first += x, v.second += x; }
+template<class U, class V, class T> inline void operator-=(std::pair<U, V>& v, const T x) { v.first -= x, v.second -= x; }
+template<class U, class V, class T> inline void operator*=(std::pair<U, V>& v, const T x) { v.first *= x, v.second *= x; }
+template<class U, class V, class T> inline void operator/=(std::pair<U, V>& v, const T x) { v.first /= x, v.second /= x; }
 
 template<class U, class V, class A, class B>
 inline std::pair<U, V> operator+(const std::pair<U, V>& l, const std::pair<A, B>& r) { return {l.first + r.first, l.second + r.second}; }
@@ -26,10 +26,21 @@ inline std::pair<U, V> operator*(const std::pair<U, V>& l, const std::pair<A, B>
 template<class U, class V, class A, class B>
 inline std::pair<U, V> operator/(const std::pair<U, V>& l, const std::pair<A, B>& r) { return {l.first / r.first, l.second / r.second}; }
 
-template<class U, class V, class A, class B> inline void operator+=(std::pair<U, V>& l, const std::pair<A, B>& r) { l = l + r; }
-template<class U, class V, class A, class B> inline void operator-=(std::pair<U, V>& l, const std::pair<A, B>& r) { l = l - r; }
-template<class U, class V, class A, class B> inline void operator*=(std::pair<U, V>& l, const std::pair<A, B>& r) { l = l * r; }
-template<class U, class V, class A, class B> inline void operator/=(std::pair<U, V>& l, const std::pair<A, B>& r) { l = l / r; }
+template<class U, class V, class A, class B> inline void operator+=(std::pair<U, V>& l, const std::pair<A, B>& r) {
+  l.first += r.first, l.second += r.second;
+}
+template<class U, class V, class A, class B> inline void operator-=(std::pair<U, V>& l, const std::pair<A, B>& r) {
+  l.first -= r.first, l.second -= r.second;
+}
+template<class U, class V, class A, class B> inline void operator*=(std::pair<U, V>& l, const std::pair<A, B>& r) {
+  l.first *= r.first, l.second *= r.second;
+}
+template<class U, class V, class A, class B> inline void operator/=(std::pair<U, V>& l, const std::pair<A, B>& r) {
+  l.first /= r.first, l.second /= r.second;
+}
+
+template<class U, class V> inline std::tuple<U, V> as_tuple(const std::pair<U, V>& p) { return {p.first, p.second}; }
+template<class U, class V> inline std::pair<U, V> from_tuple(const std::tuple<U, V>& t) { return {std::get<0>(t), std::get<1>(t)}; }
 
 template<class U, class V>
 void print_pretty_pair(std::ostream& out, const std::pair<U, V>& p, const bool new_line = true) {
