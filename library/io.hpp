b460779126d17eeaa19290ad38fd5b496bebb3dc
Omar Abdelrahman <omar.sebres@gmail.com>
2024-09-05 21:16:04 +0200

    refactor io

--- library/io.hpp
+++ library/io.hpp
@@ -1,23 +1,20 @@
-template<typename T>
-inline void read(std::istream& in, T& t) { in >> t; }
-template<typename H, typename... T>
-inline void read(std::istream& in, H& h, T&... t) {
-  read(in, h);
-  read(in, t...);
+template<typename... Ts>
+inline void read(std::istream& in, Ts&... ts) {
+  (in >> ... >> ts);
 }
 template<typename T>
-inline void print_vertically(std::ostream& out, const T& t) { out << t << '\n'; }
+inline void print(std::ostream& out, const T& t) { out << t << '\n'; }
 template<typename H, typename... T>
-inline void print_vertically(std::ostream& out, const H& h, const T&... t) {
-  print_vertically(out, h);
-  print_vertically(out, t...);
+inline void print(std::ostream& out, const H& h, const T&... t) {
+  print(out, h);
+  print(out, t...);
 }
 template<typename T>
-inline void print_horizontally(std::ostream& out, const T& t) { out << t; }
+inline void print_one_line(std::ostream& out, const T& t) { out << t; }
 template<typename H, typename... T>
-inline void print_horizontally(std::ostream& out, const H& h, const T&... t) {
-  print_horizontally(out, h);
+inline void print_one_line(std::ostream& out, const H& h, const T&... t) {
+  print_one_line(out, h);
   out << ' ';
-  print_horizontally(out, t...);
+  print_one_line(out, t...);
   if (sizeof...(t) == 1) out << '\n';
 }
